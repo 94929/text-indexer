bce02e7c7aadc7d23160023eb05f08d3d0c20bba
jsung5381 <jsung5381@naver.com>
2016-12-26 22:48:08 +0900

    Implemented indexDocs and indexDoc methods

--- src/Indexer.java
+++ src/Indexer.java
@@ -1,13 +1,18 @@
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.*;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Date;
 
 /**
@@ -27,7 +32,7 @@ public class Indexer {
 
         Date start = new Date();    // need this to get time taken.
         try {
-            System.out.println("Indexing to directory '"+indexPath+"'...");
+            System.out.println("Indexing to directory '" + indexPath + "'...");
             // param 1
             Directory dir = FSDirectory.open(Paths.get(indexPath));
 
@@ -40,10 +45,48 @@ public class Indexer {
             IndexWriter writer = new IndexWriter(dir, config);
 
             // main algo
-            // indexDocs(writer, docDir);
+            indexDocs(writer, docDir);
 
-            System.out.println((new Date().getTime()-start.getTime()) +
+            System.out.println((new Date().getTime() - start.getTime()) +
                     " total milliseconds");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /* Iterate through the paths(ie. the files) */
+    public static void indexDocs(final IndexWriter writer, Path path)
+            throws IOException {
+        if (Files.isDirectory(path)) {
+            Files.walkFileTree(path, new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult visitFile(
+                        Path file,
+                        BasicFileAttributes attrs) throws IOException {
+                    indexDoc(writer, file, attrs.lastModifiedTime().toMillis());
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        }
+    }
+
+    public static void indexDoc(
+            IndexWriter writer,
+            Path file,
+            long lastModified) throws IOException {
+
+        try (InputStream stream = Files.newInputStream(file)) {
+            Document doc = new Document();
+
+            doc.add(new StringField("path", file.toString(), Field.Store.YES));
+            doc.add(new LongPoint("modified", lastModified));
+            doc.add(new TextField("contents",
+                    new BufferedReader(
+                            new InputStreamReader(
+                                    stream, StandardCharsets.UTF_8))));
+
+            System.out.println("adding"+file);
+            writer.addDocument(doc);
         } catch (Exception e) {
             e.printStackTrace();
         }
